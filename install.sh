11e91ffc5fe48c24854c3256b02bf81250a5a9e7
madebypixel02 <madebypixel02@gmail.com>
2021-09-12 11:55:40 +0200

    Add powerlevel10k config file

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Copy vimrc, zshrc, bashrc, etc
-cp Config\ Files/.vimrc Config\ Files/.zshrc Config\ Files/.bashrc Config\ Files/.mygreeting ~/
+cp Config\ Files/.vimrc Config\ Files/.zshrc Config\ Files/.bashrc Config\ Files/.mygreeting Config\ Files/.p10k.zsh ~/
 
 # Clone & install yay
 git clone https://aur.archlinux.org/yay-bin.git ~/yay-bin

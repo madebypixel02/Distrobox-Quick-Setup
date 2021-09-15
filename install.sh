773e40524dbab8fa34a2e31548db9fa391fa584e
madebypixel02 <madebypixel02@gmail.com>
2021-09-15 19:57:33 +0200

    Fixed issue with zshrc being changed to zshrc.pre-oh-my-zsh

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+mv -f ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 # Fzf installation
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
